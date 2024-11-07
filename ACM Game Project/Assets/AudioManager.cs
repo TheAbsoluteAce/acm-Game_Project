c494053bd3f2052edf1e4c4333e0ea89039333cf
Antonio A Escobar Rodriguez <aescobarrodriguez@unr.edu>
2024-11-06 17:57:09 -0800

    bug fixes and polishing

--- ACM Game Project/Assets/AudioManager.cs
+++ ACM Game Project/Assets/AudioManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    public static AudioManager instance;
+    private static AudioManager instance;
 
     public AudioSource source;
      void Awake()
--- ACM Game Project/Assets/PlayerController.cs
+++ ACM Game Project/Assets/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -10,18 +11,24 @@ public class PlayerController : MonoBehaviour
     public SpriteRenderer sr;
     public AudioClip JumpClip;
     public AudioClip winClip;
+    public int maxJumps;
+    private int jumpCount;
+    private Animator animator;
 
     
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        jumpCount = maxJumps;
     }
 
     
     void Update()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
+        animator.SetFloat("HorizontalSpeed", Mathf.Abs(moveHorizontal));
 
         if (moveHorizontal < 0)
         {
@@ -32,10 +39,12 @@ public class PlayerController : MonoBehaviour
             sr.flipX = false;  
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (jumpCount > 0 && Input.GetKeyDown(KeyCode.Space))
         {  
             rb.velocity = new Vector2(moveHorizontal * speed, jumpRate);
+            jumpCount--;
             AudioManager.PlaySound(JumpClip);
+            
         }
         else
         {
@@ -43,11 +52,21 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("ground"))
+        {
+            Debug.Log("Hit ground");
+            jumpCount = maxJumps;
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Dead Zone"))
         {
             Debug.Log("YOU LOSE!");
+            SceneManager.LoadScene("Main Menu");
+
         }  else if (collision.CompareTag("Cherry"))
         {
             Debug.Log("YOU WIN!");
